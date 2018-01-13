21985cc2d3c371f1fab20dbb0d6b43336d487bfd
growuptang <1984123951@qq.com>
2018-01-13 13:46:53 +0800

    RecyclerView版本的GridView可进入子界面进行相关操作

---
+++ Desktop/Demo/app/src/main/java/smart/com/demo/child/SecondActivity.java
@@ -0,0 +1,25 @@
+package smart.com.demo.child;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import smart.com.demo.R;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    Button MButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        MButton = findViewById(R.id.button_backward);
+        MButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
+    }
+}
